6059468df3970620f2456573976ec6e210436d6f
François PASINI <xif6@users.noreply.github.com>
2022-07-02 19:01:26 +0200

    Update gmail.js

--- gmail.js
+++ gmail.js
@@ -1,1 +1,22 @@
+// ==UserScript==
+// @name         Utiliser Supprimer dans gmail
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://mail.google.com/mail/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @updateURL    https://github.com/xif6/tampermonkey/blob/main/gmail.js
+// @downloadURL  https://github.com/xif6/tampermonkey/blob/main/gmail.js
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    document.addEventListener('keyup', (e) => {
+        if (e.key === 'Delete' && e.target.nodeName !== 'INPUT' && !e.target.isContentEditable) {
+            document.dispatchEvent(new KeyboardEvent('keypress', {'key': '#'}));
+        }
+    });
+
+})();
